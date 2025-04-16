296a2c20e9d3205ed4b8497d871ab816531096da
elizabethwiggins <ewiggins@unomaha.edu>
2025-04-16 22:29:12 +0000

    "Address already in use"

--- BoardGame.py
+++ BoardGame.py
@@ -1,23 +1,59 @@
-#Example Flask App for a hexaganal tile game
-#Logic is in this python file
-
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, session, request, redirect, url_for
+import random
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
-# Keep track of the current player
-current_player = 1
+# Initialize game board
+spaces = ["Enter"] + ["Space " + str(i) for i in range(2, 20)] + ["Finish"]
+
+# Randomly integrate ghosts
+def initialize_game():
+    evil_ghosts = random.sample(range(1, 19), 3)
+    friendly_ghosts = random.sample([x for x in range(1, 19) if x not in evil_ghosts], 3)
+    return evil_ghosts, friendly_ghosts
+
+evil_ghosts, friendly_ghosts = initialize_game()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    session['player1_pos'] = 0
+    session['player2_pos'] = 0
+    session['turn'] = 1
+    return render_template('game.html', spaces=spaces, evil_ghosts=evil_ghosts, friendly_ghosts=friendly_ghosts)
 
-@app.route('/add_tile')
-def add_tile():
-    global current_player
-    color = 'green' if current_player == 1 else 'blue'  # Alternate between green and blue
-    current_player = 2 if current_player == 1 else 1  # Switch to the other player
-    return jsonify(color=color)
+@app.route('/roll', methods=['POST'])
+def roll():
+    dice_roll = random.randint(1, 6)
+    if session['turn'] == 1:
+        session['player1_pos'] = min(session['player1_pos'] + dice_roll, 19)
+        pos = session['player1_pos']
+        if pos in friendly_ghosts:
+            session['player1_pos'] = min(session['player1_pos'] + random.randint(1, 6), 19)
+        elif pos in evil_ghosts:
+            session['player1_pos'] = max(session['player1_pos'] - random.randint(1, 6), 0)
+        if pos == 19:
+            session['winner'] = "Player 1 wins!"
+            return redirect(url_for('end_game'))
+        session['turn'] = 2
+    else:
+        session['player2_pos'] = min(session['player2_pos'] + dice_roll, 19)
+        pos = session['player2_pos']
+        if pos in friendly_ghosts:
+            session['player2_pos'] = min(session['player2_pos'] + random.randint(1, 6), 19)
+        elif pos in evil_ghosts:
+            session['player2_pos'] = max(session['player2_pos'] - random.randint(1, 6), 0)
+        if pos == 19:
+            session['winner'] = "Player 2 wins!"
+            return redirect(url_for('end_game'))
+        session['turn'] = 1
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
+@app.route('/end')
+def end_game():
+    winner = session.get('winner', '')
+    return render_template('end.html', winner=winner)
+
+if __name__ == '__main__':
     app.run(debug=True)
+
